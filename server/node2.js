29276081e168fb72ad9af5e7a2ded1f92db789e5
Christos Vontas <christos@redsift.io>
2016-10-12 19:07:33 +0100

    remove double exporting, count is not a true total anymore

--- server/node2.js
+++ server/node2.js
@@ -1,21 +1,13 @@
 module.exports = function (got) {
   const json = got.in.data.map(d => JSON.parse(d.value));
 
-  const total = json.reduce((p, d) => p + d.count, 0);
-
   const query = got.query;
   const threadId = query[0];
   let newestCount = json
     .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
     .shift().count;
 
-  return [
-    {
-      name: 'count',
-      key: 'word_count',
-      value: total
-    },
-    {
+  return [{
       name: 'threads',
       key: threadId,
       value: {
